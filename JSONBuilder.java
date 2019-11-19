85bff6ce46ec660dbd0d55274cda7de5ae2f3cb0
YuraVolk <42916748+YuraVolk@users.noreply.github.com>
2019-11-19 17:39:09 +0300

    Add files via upload

--- JSONBuilder.java
+++ JSONBuilder.java
@@ -4,9 +4,11 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class JSONBuilder implements Builder {
+    private String result;
+
     @Override
     public void createElement(String element) {
-        Pattern pattern = Pattern.compile("(?<=<).+(?=>.)");
+        Pattern pattern = Pattern.compile("(?<=<).+(?=/?>.)");
         Matcher matcher = pattern.matcher(element);
         String name = "";
         String content = "";
@@ -24,15 +26,25 @@ public class JSONBuilder implements Builder {
             if (matcher.find()) {
                 content = matcher.group();
             }
-            System.out.println(name);
-            System.out.println(content);
-        } else {
 
+            result = String.format("{\"%s\":\"%s\"}",
+                    name, content);
+        } else {
+            pattern = Pattern.compile("(?<=<).+(?=/>)");
+            matcher = pattern.matcher(element);
+
+            if (matcher.find()) {
+                name = matcher.group();
+            }
+
+            result = String.format("{\"%s\":\"null\"}",
+                    name);
         }
+        print();
     }
 
     @Override
     public void print() {
-
+        System.out.println(result);
     }
 }
